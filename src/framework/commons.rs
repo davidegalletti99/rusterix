d225efed2a8052db2dca1039e4a9aceefdde6f5e
Davide Galletti <davide.galletti@leonardo.com>
2025-09-14 23:17:54 +0200

    fix: now the fixed lenght pass every test
    Changed the fixed_length's set_value() function, now the offsets refers
    to the number of bits to leave to the left of the passed value and the
    mask refers to the mask to apply over the passed value in order to apply
    the passed value to the buffer

--- src/framework/commons.rs
+++ src/framework/commons.rs
@@ -1,2 +1,12 @@
 pub const BYTE_SIZE: usize = 8;
 pub type MaskType = u128;
+
+pub trait SerializeDeserialize {
+    fn serialize(&self) -> Vec<u8>;
+    fn deserialize(data: &[u8]) -> Self where Self: Sized;
+}
+
+pub trait Field { 
+    fn size_bytes(&self) -> usize;
+    fn data_existing(&self) -> bool;
+}
